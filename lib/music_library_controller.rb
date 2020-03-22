962f77b09e4f95626551df01225aedb60df834b4
Mitzi Estrada <m_estrada17@live.com>
2020-03-22 06:17:26 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,8 +24,12 @@ class MusicLibraryController
   end
   
   def list_songs
-    
-  end 
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
+  end
+  
+  
   
   
   
